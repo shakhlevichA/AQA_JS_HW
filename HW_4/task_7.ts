ec7cbfd13754b41a2ec0261b6b9c051e63079ae0
Andrey <110105192+shakhlevichA@users.noreply.github.com>
2023-02-21 15:21:15 +0300

    Delete HW_4 directory

--- HW_4/task_7.ts
+++
@@ -1,8 +0,0 @@
-//Дана строка
-// '                я учу typescript!                '. 
-//Выведите строку, без начальных и пробелов в конце строки.
-
-
-const givStr = '                я учу typescript!                ';
-
-console.log(givStr.trim())
